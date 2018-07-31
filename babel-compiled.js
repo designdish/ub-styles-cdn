af6f4d7ff3cc0157b01df5ab8d8023fada13abd7
Bowie <bowie.leidy@gmail.com>
2018-07-31 18:15:59 -0400

    Committing changes that include compiled babel

--- babel-compiled.js
+++ babel-compiled.js
@@ -1,18 +1,19 @@
+'use strict';
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 var to, from, modalTrigger, modalFired, sessionInfo, licenseInfo, licenseType, licenseVersion, offerModal, parsedURL;
 
 modalFired = false;
 var today = new Date();
 var daily = new Date(today.getTime() + 1);
 
-
 var userLicenseInputContainer = document.getElementsByClassName('updateWishContainer')[0],
     unbounceModalContainer = document.getElementsByClassName('ub-emb-iframe-wrapper')[0],
     modalDismissal = document.querySelector('.highslide-dimming'),
     proposalDismissal = document.querySelector('.proposalClose');
 
-
-
-var setCookie = function(cName, cValue, cExpires, cPath) {
+var setCookie = function setCookie(cName, cValue, cExpires, cPath) {
     if (!cPath) {
         cPath = "/";
     }
@@ -22,17 +23,17 @@ var setCookie = function(cName, cValue, cExpires, cPath) {
     document.cookie = cName + "=" + cValue + ";expires=" + cExpires.toUTCString() + "; path=" + cPath;
 };
 
-var listenForSelection = function() {
+var listenForSelection = function listenForSelection() {
     var buyButton = document.querySelectorAll('.usBuyButton');
     addEvent('click', buyButton, injectNewPromoCode);
-}
+};
 
-var updateCookie = function(cName, cValue) {
+var updateCookie = function updateCookie(cName, cValue) {
     var expireDate = document.cookie.indexOf(cName) === -1 ? new Date(new Date().setTime(new Date().getTime() + 30 * 24 * 3600 * 1000)) : unescape(document.cookie).split('expireDate=')[1];
     document.cookie = cName + '=' + cValue + ',expireDate=' + expireDate + ';expires=' + expireDate;
 };
 
-var getCookie = function(cName) {
+var getCookie = function getCookie(cName) {
     var cStr = document.cookie;
 
     var startSlice = cStr.indexOf(cName + "=");
@@ -51,29 +52,26 @@ var getCookie = function(cName) {
 };
 
 // check our url for licensing 
-var getParameterByName = function(name, url) {
-    if (!url)
-        url = window.location.href;
+var getParameterByName = function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
     // var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results)
-        return null;
-    if (!results[2])
-        return '';
+    if (!results) return null;
+    if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
 
 // create a session info object
-var getSessionInfo = function() {
+var getSessionInfo = function getSessionInfo() {
     var userInputLicenseNumber = userLicenseInputContainer.querySelector('input[type="text"]').value;
     sessionInfo = {
         customOrder: getParameterByName('CustomOrder', url),
         country: getParameterByName('Countryiso', url),
-        licenseNumber: (getParameterByName('license', url) == null) ? userInputLicenseNumber.replace(/\s/g, "") : getParameterByName('license', url).replace(/\s/g, ""),
+        licenseNumber: getParameterByName('license', url) == null ? userInputLicenseNumber.replace(/\s/g, "") : getParameterByName('license', url).replace(/\s/g, ""),
         coupon: getParameterByName('coupon', window.location.href),
-        OneOptionRedirect: getParameterByName('OneOptionRedirect', url),
+        OneOptionRedirect: getParameterByName('OneOptionRedirect', url)
     };
     console.log(sessionInfo);
 
@@ -87,11 +85,10 @@ var getSessionInfo = function() {
     }
 };
 
-var composeURL = function(params) {
+var composeURL = function composeURL(params) {
     var entryURL = window.location.href;
     parsedURL = entryURL.replace(window.location.origin, "");
     parsedURL = parsedURL.replace(window.location.pathname, "");
-
 
     if (parsedURL.indexOf(params.coupon)) {
         parsedURL = parsedURL.replace('coupon=' + params.coupon, "");
@@ -102,48 +99,47 @@ var composeURL = function(params) {
         parsedURL += "&license=" + params.licenseNumber;
         console.log('license ' + params.licenseNumber + ' successfully injected into url');
     }
-
 };
 
 //replace an existing input field in the form with our new coupon code. 
 
-var updateValue = function(el, value) {
+var updateValue = function updateValue(el, value) {
     el.value = value;
     __doPostBack(el, value);
 };
 
-var injectNewPromoCode = function() {
+var injectNewPromoCode = function injectNewPromoCode() {
     var couponBox = document.getElementById('UpdateShop1_couponTextBox');
     var newPromo = getParameterByName('coupon');
     var currentPromo = couponBox.value;
 
-    if ((newPromo != currentPromo) && (newPromo != null)) {
+    if (newPromo != currentPromo && newPromo != null) {
         updateValue(couponBox, newPromo);
     }
 };
 
 // Pass our license to the address params
-var updateAddress = function() {
+var updateAddress = function updateAddress() {
     var newURL = "update.aspx?license=" + sessionInfo.licenseNumber;
     window.history.pushState(sessionInfo, "updateLicense", newURL);
 };
 
 // update our URL if there is no license passed through
-var updateURLWithLicenseParams = function() {
+var updateURLWithLicenseParams = function updateURLWithLicenseParams() {
     if (getParameterByName('license', url) == null) {
         updateAddress();
     }
 };
 
-var postMessageToIframe = function(message) {
-    setTimeout(function() {
+var postMessageToIframe = function postMessageToIframe(message) {
+    setTimeout(function () {
         var unbounceModal = unbounceModalContainer.querySelector('iframe');
         unbounceModal.contentWindow.postMessage(message, '*');
     }, 750);
 };
 
 //cross browser event handling (firefox doesn't like to recognize window events by default)
-var addEvent = function(obj, evt, fn) {
+var addEvent = function addEvent(obj, evt, fn) {
     if (obj.addEventListener) {
         obj.addEventListener(evt, fn, false);
     } else if (obj.attachEvent) {
@@ -152,7 +148,7 @@ var addEvent = function(obj, evt, fn) {
 };
 
 // inject modal triggering element - we're going to exploit the click option within unbounce to trigger a modal when the user leaves. 
-var injectModalTrigger = function() {
+var injectModalTrigger = function injectModalTrigger() {
     var modalButton = document.createElement('button');
     modalTrigger = modalButton;
     modalButton.style.display = 'none';
@@ -160,11 +156,11 @@ var injectModalTrigger = function() {
     document.getElementsByTagName('body')[0].appendChild(modalButton);
 };
 
-var resetModal = function() {
+var resetModal = function resetModal() {
     modalFired = !modalFired;
 };
 
-var checkForExistingSubscription = function() {
+var checkForExistingSubscription = function checkForExistingSubscription() {
     if (subscriptionLabel) {
         modalFired = true;
     } else {
@@ -173,9 +169,9 @@ var checkForExistingSubscription = function() {
 };
 
 // Injecting script to detect when the user exits the window. 
-var injectExitDetection = function() {
+var injectExitDetection = function injectExitDetection() {
     if (modalFired === false) {
-        addEvent(document, 'mouseout', function(e) {
+        addEvent(document, 'mouseout', function (e) {
             e = e ? e : window.event;
             from = e.relatedTarget || e.toElement;
             if (!from || from.nodeName == "HTML") {
@@ -188,9 +184,9 @@ var injectExitDetection = function() {
 };
 
 // Injecting script to check if the user re-enters the window
-var injectEntranceDetection = function() {
+var injectEntranceDetection = function injectEntranceDetection() {
     if (modalFired === false) {
-        addEvent(document, 'mouseover', function(e) {
+        addEvent(document, 'mouseover', function (e) {
             e = e ? e : window.event;
             from = e.relatedTarget || e.toElement;
             if (from || from.nodeName == "HTML") {
@@ -203,17 +199,17 @@ var injectEntranceDetection = function() {
 
 var exitTimer;
 
-var startExitTimer = function() {
+var startExitTimer = function startExitTimer() {
     exitTimer = setTimeout(displayModal, 750);
 };
 
-var displayModalOnOfferDismissal = function() {
+var displayModalOnOfferDismissal = function displayModalOnOfferDismissal() {
     addEvent(modalDismissal, 'click', delayedModalDisplay(750));
     addEvent(proposalDismissal, 'click', delayedModalDisplay(750));
 };
 
 // display the modal 
-var displayModal = function() {
+var displayModal = function displayModal() {
     if (!modalFired) {
         setCookie("retention-score", 1, daily);
         unbounceModalContainer = document.getElementsByClassName('ub-emb-iframe-wrapper')[0];
@@ -226,29 +222,28 @@ var displayModal = function() {
     }
 };
 
-var delayedModalDisplay = function(timer) {
+var delayedModalDisplay = function delayedModalDisplay(timer) {
     setTimeout(displayModal, timer);
 };
 
-
-var manualUpgradeCheck = function() {
+var manualUpgradeCheck = function manualUpgradeCheck() {
     var userInputButton = userLicenseInputContainer.querySelector('.updateWishButton');
     // setTimeout(function(){addEvent(userInputButton, 'click', checkForUpgrades);}, 2500);
     addEvent(userInputButton, 'click', checkForUpgrades);
 };
 
-var checkForUpgrades = function() {
-    setTimeout(function() {
+var checkForUpgrades = function checkForUpgrades() {
+    setTimeout(function () {
         offerModal = document.getElementById('my-content');
         checkForElement(offerModal, 3E4, runUpgradeProcess);
     }, 2500);
     // checkForElement(offerModal, 3E4, runUpgradeProcess);
 };
 
-var waitForModal = function() {
+var waitForModal = function waitForModal() {
     var offeredDiscount = getCookie("retention-score");
     if (!offeredDiscount) {
-        offerModal = (document.getElementById('my-content')) ? document.getElementById('my-content') : 0;
+        offerModal = document.getElementById('my-content') ? document.getElementById('my-content') : 0;
         if (offerModal === 0) {
             manualUpgradeCheck();
         } else {
@@ -257,38 +252,51 @@ var waitForModal = function() {
     }
 };
 
-var checkForElement = function(element, time, fn) {
-    // console.warn(element, time, fn);
-    var waitForElement = timeoutms => new Promise(() => {
-        // console.warn('checking for ' + element);
+var checkForElement = function checkForElement(element, time, fn) {
+    var _this = this;
 
-        var check = () => {
-            if ((element.length > 0) || (element.innerHTML.length)) {
-                // console.log(element + ' is ready, that only took ' + time + 'ms');
-                fn();
-            } else if ((timeoutms -= 100) < 0) {
-                console.log("element " + element + " not available!");
-            } else {
-                setTimeout(check, 100);
+    // console.warn(element, time, fn);
+    var waitForElement = function waitForElement(timeoutms) {
+        return new Promise(function () {
+            // console.warn('checking for ' + element);
+
+            var check = function check() {
+                if (element.length > 0 || element.innerHTML.length) {
+                    // console.log(element + ' is ready, that only took ' + time + 'ms');
+                    fn();
+                } else if ((timeoutms -= 100) < 0) {
+                    console.log("element " + element + " not available!");
+                } else {
+                    setTimeout(check, 100);
+                }
+            };
+            setTimeout(check, 100);
+        });
+    };
+    setTimeout(function () {
+        element;
+    }, time / 10);
+    _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        waitForElement(time);
+
+                    case 1:
+                    case 'end':
+                        return _context.stop();
+                }
             }
-        };
-        setTimeout(check, 100);
-    });
-    setTimeout(() => { element }, (time / 10));
-    (async() => {
-        waitForElement(time);
-    })();
+        }, _callee, _this);
+    }))();
 };
 
-var runUpgradeProcess = function() {
+var runUpgradeProcess = function runUpgradeProcess() {
 
     if (offerModal != null) {
         licenseInfo = document.querySelector('[data-version]');
-        licenseType = document.querySelector('[data-licensetype]').dataset.licensetype,
-            licenseVersion = document.querySelector('[data-version]').dataset.version;
-
-
-
+        licenseType = document.querySelector('[data-licensetype]').dataset.licensetype, licenseVersion = document.querySelector('[data-version]').dataset.version;
 
         if (licenseVersion != -1) {
             injectModalTrigger();
@@ -297,7 +305,7 @@ var runUpgradeProcess = function() {
             checkForElement(userLicenseInputContainer, 3E4, getSessionInfo);
         }
     }
-}
+};
 
 try {
     waitForModal();
