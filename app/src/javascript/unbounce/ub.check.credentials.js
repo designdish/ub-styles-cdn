486504028fa8e890e6852587eff03fd62c877664
Bowie <bowie.leidy@gmail.com>
2018-08-07 17:19:45 -0400

    committing md5 hash minified

---
+++ app/src/javascript/unbounce/ub.check.credentials.js
@@ -0,0 +1,24 @@
+var mId = 18045513; //mailid
+var utmC = 'news2018-Q3-August-Mig-MeetingUsers-T1-enUS'; //utm_campaign
+var hash = md5(campaignId + utm_campaign);
+
+var checkCredentials = function(token, hash) {
+    if (token != hash) {
+        window.location('http://teamviewer.us');
+    }
+};
+
+var user = {
+    firstName: getParameterByName('first'),
+    lastName: getParameterByName('last'),
+    email: getParameterByName('email')
+};
+
+var injectUserInfo = function(el, str) {
+    el.innerText = str;
+};
+
+window.onload = function() {
+    var token = getParameterByName(utm_campaign) + getParameterByName(mailId);
+    checkCredentials(token);
+};
