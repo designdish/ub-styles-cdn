7f04b1e5aff8dc6537149a6bcfc747e2222eebe5
Bowie <bowie.leidy@gmail.com>
2018-08-07 18:27:44 -0400

    removing identify links shit from the push

--- app/dist/scripts/ub.bundle.js
+++ app/dist/scripts/ub.bundle.js
@@ -463,7 +463,6 @@ var handleOutboundLinkClicks = function(event) {
 
 
 addEvent('document', 'DOMContentLoaded', (function() {
-    setTimeout(identifyLinks(), 1500);
     var token = getParameterByName('utm_campaign') + getParameterByName('mailId');
     var mId = 18045513; //mailid
     var utmC = 'news2018-Q3-August-Mig-MeetingUsers-T1-enUS'; //utm_campaign
