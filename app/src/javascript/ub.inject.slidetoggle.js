7efba3b2a82951244ae41642c9012cf383e468e8
Bowie <bowie.leidy@gmail.com>
2018-08-07 11:42:46 -0400

    committing slide toggle changes for the unbounce form.

---
+++ app/src/javascript/ub.inject.slidetoggle.js
@@ -0,0 +1,10 @@
+var injectSlideToggle = function() {
+    var radioGroup = document.getElementsByClassName('hs-fieldtype-radio');
+    var switchIndicator = document.createElement('div');
+    switchIndicator.classList.add('switch__indicator');
+    radioGroup[0].insertAdjacentElement('beforeend', switchIndicator);
+};
+
+window.onload = function() {
+    injectSlideToggle();
+}
