4ade22a071d56ed7dd6810155fee319fb46b9672
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-10 21:44:24 -0400

    moving the parameters function

--- app/src/javascript/unbounce/ub.check.credentials.js
+++ app/src/javascript/unbounce/ub.check.credentials.js
@@ -1,13 +1,3 @@
-var getParameterByName = function(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return "";
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-};
-
 var mId = 18045513; //mailid
 var utmC = "news2018-Q3-August-Mig-MeetingUsers-T1-enUS"; //utm_campaign
 var hash = md5(mId + utmC);
