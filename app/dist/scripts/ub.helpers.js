e63f492b58e4b71ae12ec0ee8f941ab5da3ed964
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-11 19:06:44 -0400

    pusing changes to slide show to concatenated helpers file

--- app/dist/scripts/ub.helpers.js
+++ app/dist/scripts/ub.helpers.js
@@ -39,10 +39,14 @@ var getUser = function() {
 	return user;
 };
 
-function showSlides(slides, time) {
+function showSlides(slides, time, cl, inClass, outClass) {
 	var slideIndex = 0;
 	var i;
 	for (i = 0; i < slides.length; i++) {
+		if (i > 0) {
+			slides[i].classList.remove.apply(cl, inClass);
+			slides[i].classList.add.apply(cl, outClass);
+		}
 		slides[i].style.display = "none";
 	}
 	slideIndex++;
@@ -50,6 +54,8 @@ function showSlides(slides, time) {
 		slideIndex = 1;
 	}
 	slides[slideIndex - 1].style.display = "block";
+	slides[i].classList.add.apply(cl, inClass);
+
 	setTimeout(showSlides, time);
 }
 
