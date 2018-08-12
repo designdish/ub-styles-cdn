d963a372d7449f0539a5ceada31d7facbc1ad0da
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-11 23:34:48 -0400

    changing the array indexing

--- app/src/javascript/unbounce/helpers/slideshow.js
+++ app/src/javascript/unbounce/helpers/slideshow.js
@@ -1,31 +1,31 @@
-function showSlides(slides, time, inClass, outClass) {
+var showSlides = function(slides, time, inClass, outClass) {
 	var slideIndex;
+	var slideIndex = 0;
 
 	function sliding() {
 		var i, cl;
-		var slideIndex = 0;
 
 		for (i = 0; i < slides.length; i++) {
 			cl = slides[i].classList;
 			slides[i].style.display = "none";
 			if (i > 0) {
 				slides[i].classList.remove.apply(cl, inClass);
-				slides[i].classList.add.apply(cl, outClass);
 			}
 		}
 		slideIndex++;
 		if (slideIndex > slides.length) {
 			slideIndex = 1;
 		}
-		slides[slideIndex - 1].style.display = "block";
-		slides[slideIndex - 1].classList.add.apply(cl, inClass);
+		var currentSlideIndex = slideIndex - 1;
+		slides[currentSlideIndex].style.display = "block";
+		// 		slides[slideIndex - 1].classList.remove.apply(cl, outClass);
+		// 		slides[slideIndex - 1].classList.add.apply(cl, inClass);
 
-		if (i > [slideIndex - 1]) {
-			setTimeout(sliding, time);
-		} else {
+		if (i < [currentSlideIndex]) {
 			var loader = document.getElementsByClassName("loader");
-			setTimeout(removeEl(loader), 1500);
+			removeEl(loader);
+		} else {
+			setTimeout(sliding, time);
 		}
 	}
-	sliding();
-}
+};
