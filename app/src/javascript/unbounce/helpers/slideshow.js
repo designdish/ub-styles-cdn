81c5d3e5718020a8f7f48fa4f3d7483d84517153
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-11 22:19:20 -0400

    edits

--- app/src/javascript/unbounce/helpers/slideshow.js
+++ app/src/javascript/unbounce/helpers/slideshow.js
@@ -1,25 +1,31 @@
 function showSlides(slides, time, inClass, outClass) {
-	var slideIndex = 0;
-	var i, cl;
-	for (i = 0; i < slides.length; i++) {
-		cl = slides[i].classList;
-		if (i > 0) {
-			slides[i].classList.remove.apply(cl, inClass);
-			slides[i].classList.add.apply(cl, outClass);
-		}
-		slides[i].style.display = "none";
-	}
-	slideIndex++;
-	if (slideIndex > slides.length) {
-		slideIndex = 1;
-	}
-	slides[slideIndex - 1].style.display = "block";
-	slides[slideIndex - 1].classList.add.apply(cl, inClass);
+	var slideIndex;
 
-	if (i < slideIndex - 1) {
-		setTimeout(showSlides, time);
-	} else {
-		var loader = document.getElementsByClassName("loader");
-		setTimeout(removeEl(loader), 1500);
+	function sliding() {
+		var i, cl;
+		var slideIndex = 0;
+
+		for (i = 0; i < slides.length; i++) {
+			cl = slides[i].classList;
+			slides[i].style.display = "none";
+			if (i > 0) {
+				slides[i].classList.remove.apply(cl, inClass);
+				slides[i].classList.add.apply(cl, outClass);
+			}
+		}
+		slideIndex++;
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+		slides[slideIndex - 1].style.display = "block";
+		slides[slideIndex - 1].classList.add.apply(cl, inClass);
+
+		if (i > [slideIndex - 1]) {
+			setTimeout(sliding, time);
+		} else {
+			var loader = document.getElementsByClassName("loader");
+			setTimeout(removeEl(loader), 1500);
+		}
 	}
+	sliding();
 }
