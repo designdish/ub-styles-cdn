ecf5bd15ff13d506addfa78c51e6d698e3c95a93
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-13 14:29:55 -0400

    pushing dev scripts for testing

---
+++ app/dist/scripts/ub.bundle.dev.js
@@ -0,0 +1,92 @@
+var load = (function() {
+    function _load(tag) {
+        return function(url) {
+            return new Promise(function(resolve, reject) {
+                var element = document.createElement(tag);
+                var parent = "head";
+                var attr = "src";
+                element.onload = function() {
+                    resolve(url);
+                };
+                element.onerror = function() {
+                    waitForIt(url);
+                };
+                switch (tag) {
+                    case "script":
+                        element.async = true;
+                        break;
+                    case "link":
+                        element.type = "text/css";
+                        element.rel = "stylesheet";
+                        attr = "href";
+                        parent = "head";
+                }
+
+                element[attr] = url;
+                document[parent].appendChild(element);
+            });
+        };
+    }
+
+    return {
+        css: _load("link"),
+        js: _load("script"),
+        img: _load("img")
+    };
+})();
+
+var waitForIt = function(obj) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(obj);
+        }, 250);
+    });
+};
+
+var buildUrl = function(files, cdn) {
+    var urls = [];
+    for (var i = 0; files.length > i; i++) {
+        var file = "'" + cdn + files[i] + "'";
+        urls.push(file);
+    }
+    return urls;
+};
+
+var jscripts = {
+    files: [
+        "inject.loader.js",
+        "md5.js",
+        "check.credentials.js",
+        "get.user.js"
+    ],
+    cdn:
+        "https://rawgit.com/designdish/ub-styles-cdn/master/app/src/javascript/unbounce/ub."
+};
+
+// Promise.all([load.js(buildUrl(jscript.files, jscript.cdn))]);
+var loadHelpers = Promise.all([
+    load.js(
+        "https://rawgit.com/designdish/ub-styles-cdn/master/app/dist/scripts/ub.helpers.dev.js"
+    )
+]);
+
+var injectLoaders = Promise.all([
+    waitForIt(window.getUser).then(function() {
+        load.js(
+            "https://rawgit.com/designdish/ub-styles-cdn/master/app/src/javascript/unbounce/ub.inject.loader.js"
+        ),
+            load.js(
+                "https://rawgit.com/designdish/ub-styles-cdn/master/app/src/javascript/unbounce/ub.md5.js"
+            );
+    })
+]);
+
+loadHelpers.then(function() {
+    injectLoaders.then(function() {
+        waitForIt(window.md5).then(function() {
+            load.js(
+                "https://rawgit.com/designdish/ub-styles-cdn/master/app/src/javascript/unbounce/ub.check.credentials.js"
+            );
+        });
+    });
+});
---
+++ app/dist/scripts/ub.helpers.dev.js
@@ -0,0 +1,103 @@
+var addEvent = function(obj, evt, fn) {
+	if (obj.addEventListener) {
+		obj.addEventListener(evt, fn, false);
+	} else if (obj.attachEvent) {
+		obj.attachEvent("on" + evt, fn);
+	}
+};
+
+var animationEnd = function(el) {
+	var animations = {
+		animation: "animationend",
+		OAnimation: "oAnimationEnd",
+		MozAnimation: "mozAnimationEnd",
+		WebkitAnimation: "webkitAnimationEnd"
+	};
+	for (var t in animations) {
+		if (el.style[t] !== undefined) {
+			return animations[t];
+		}
+	}
+};
+
+var getParameterByName = function(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+var getUser = function() {
+	var user = {
+		firstName: getParameterByName("first"),
+		lastName: getParameterByName("last"),
+		email: getParameterByName("email")
+	};
+	return user;
+};
+
+var removeEl = function(el) {
+	if (el != undefined && el.parentNode.innerHTML.length > -1) {
+		el.parentNode.removeChild(el);
+	}
+};
+
+
+var showSlides = function(slides, time, inClass, outClass, slideIndex) {
+	var currentSlideIndex;
+	var loopTimer;
+	var loader = document.getElementsByClassName("loader");
+
+	var sliding = function() {
+		if (slideIndex < slides.length) {
+			var i, cl;
+
+			for (i = 0; i < slides.length; i++) {
+				cl = slides[i].classList;
+				slides[i].classList.add.apply(cl, outClass);
+				slides[i].style.display = "none";
+				if (i > 0) {
+					slides[i].classList.remove.apply(cl, outClass);
+					slides[i].classList.add.apply(cl, inClass);
+					slides[i - 1].style.display = "none";
+					slides[i].style.display = "block";
+				}
+			}
+			slideIndex = slideIndex++;
+			// if (slideIndex > slides.length) {
+			// 	slideIndex = 1;
+			// }
+			if (currentSlideIndex == undefined || slideIndex > 0) {
+				currentSlideIndex = [slideIndex - 1];
+				if ((currentSlideIndex = -1)) {
+					currentSlideIndex = 0;
+				}
+			}
+			slides[currentSlideIndex].style.display = "block";
+			slides[currentSlideIndex].classList.remove.apply(cl, outClass);
+			slides[currentSlideIndex].classList.add.apply(cl, inClass);
+
+			if (i < [currentSlideIndex]) {
+				var loader = document.getElementsByClassName("loader");
+				removeEl(loader[0]);
+			} else {
+				loopTimer = setTimeout(sliding, time);
+			}
+		}
+	};
+	if (slideIndex < slides.length) {
+		sliding();
+	} else {
+		clearTimeout(loopTimer);
+	}
+	removeEl(loader[0]);
+};
+
+var wrap = function(el, wrapper) {
+	el.parentNode.insertBefore(wrapper, el);
+	wrapper.appendChild(el);
+	return wrapper;
+};
