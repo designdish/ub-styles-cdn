4c2d76dbfd0962876a4306df9fb73e11f90a3fe0
Bowie <bowie.leidy@gmail.com>
2018-08-14 16:31:01 -0400

    refactoring parameter checking

--- app/dist/scripts/tv.helpers.js
+++ app/dist/scripts/tv.helpers.js
@@ -31,34 +31,18 @@ var animationEnd = function(el) {
 var checkParams = function(url, arr) {
     for (var i = arr.length - 1; i >= 0; i--) {
         var param = arr[i];
-        var paramVal = getParameterByName(param, url);
+        var cookieVal = getCookie(param);
+        var paramVal = (getParameterByName(param, url) === null) ? cookieVal : getParameterByName(param, url);
 
-        if (paramVal === null) {
-            paramVal = getCookie(param);
-            if (paramVal === false) {
-                // if no parameter is present in the url, set the cookie and the parameter value to default
-                setCookie(param, "");
-                paramVal = getCookie(param);
-                logCookie(param, getCookie(param));
-            }
-            //append the newly created parameter to the url
-            url = appendParam(url, param, paramVal);
-            // setCookie(param, paramVal);
-            // logCookie(param, getCookie(param));
+        if ((paramVal === null) && (cookieVal === false)) {
+            return url;
         } else {
-            // if a parameter value is present in the url, reset the cookie to reflect the parameter value
+            url = appendParam(url, param, paramVal);
             setCookie(param, paramVal);
             url = updateParam(url, param, paramVal);
-            // logCookie(param, getCookie(param));
+            return url;
         }
-        // setTimeout(function() {
-        // 	if (paramVal != getCookie(param)) {
-        // 		eraseCookie(param);
-        // 		setCookie(param, paramVal);
-        // 	}
-        // }, 1500);
     }
-    return url;
 };
 		var today = new Date();
 
