bbacd6b23e0691f41b2554f0f1759afa8a5d3a2a
Bowie <bowie.leidy@gmail.com>
2018-08-14 20:24:09 -0400

    fixing the appendages

--- app/src/javascript/unbounce/helpers/join.parameters.js
+++ app/src/javascript/unbounce/helpers/join.parameters.js
@@ -7,28 +7,30 @@ var joinParameters = function(url, baseParam, targetParam) {
     for (var i = targetParam.length - 1; i >= 0; i--) {
         //for each parameter in our target parameter array, check for a parameter or a cookie
         var target = targetParam[i],
-            targetVal = (getValue(targetParam[i]) != undefined) ? (getValue(targetParam[i])) : undefined,
-            newParam = (targetVal != undefined) ? target + "=" + targetVal : "",
+            targetVal = (getValue(targetParam[i]) != undefined) ? (getValue(targetParam[i])) : " ",
+            newParam = (targetVal != undefined) ? target + "=" + targetVal : " ",
             appendedParam = (targetVal != undefined) ? target + "-" + targetVal : target + '-';
 
         if (targetVal != undefined) {
             if (newParamVal != undefined) {
                 if (newParamVal.indexOf(appendedParam) === -1) {
+                    newParamVal = newParamVal.replace(" ", "");
                     newParamVal += "-" + appendedParam;
                 } else {
+
                     newParamVal = updateJoinedParameters(
                         newParamVal,
                         target,
-                        targetVal
+                        targetVal.replace(" ", "")
                     );
                     url = updateParam(url, target, targetVal);
                 }
             }
 
             if (url.indexOf(newParam) === -1) {
-                url = appendParam(url, target, targetVal);
+                url = appendParam(url, target, targetVal.replace(" ", ""));
             } else {
-                updateParam(url, target, targetVal);
+                updateParam(url, target, targetVal.replace(" ", ""));
             }
         } else if (targetVal === undefined) {
             newParamVal += "-" + appendedParam;
