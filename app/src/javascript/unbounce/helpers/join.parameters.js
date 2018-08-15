a432e664f5bd6751b16bda88ce0247f9b479fd63
Bowie <bowie.leidy@gmail.com>
2018-08-14 21:13:10 -0400

    committing new update to join parameters functionality

--- app/src/javascript/unbounce/helpers/join.parameters.js
+++ app/src/javascript/unbounce/helpers/join.parameters.js
@@ -2,7 +2,7 @@ var joinParameters = function(url, baseParam, targetParam) {
     var newParamVal, result, newLink, joinedParams, baseParamVal;
 
     baseParamVal = getValue(baseParam);
-    newParamVal = (baseParamVal != undefined) ? baseParamVal : undefined;
+    newParamVal = (baseParamVal != undefined) ? baseParamVal : " ";
 
     for (var i = targetParam.length - 1; i >= 0; i--) {
         //for each parameter in our target parameter array, check for a parameter or a cookie
@@ -14,30 +14,45 @@ var joinParameters = function(url, baseParam, targetParam) {
         if (targetVal != undefined) {
             if (newParamVal != undefined) {
                 if (newParamVal.indexOf(appendedParam) === -1) {
-                    newParamVal = newParamVal.replace(" ", "");
-                    newParamVal += "-" + appendedParam;
+                    if (newParamVal === " ") {
+                        newParamVal = newParamVal.replace(/\s+/g, '');
+                    } else {
+                        newParamVal += "-" + appendedParam;
+                    }
                 } else {
+                    if (targetVal === " ") {
+                        newParamVal = updateJoinedParameters(
+                            newParamVal,
+                            target,
+                            targetVal.replace(/\s+/g, '')
+                        );
+                    } else {
+                        newParamVal = updateJoinedParameters(
+                            newParamVal,
+                            target,
+                            targetVal.replace(/\s+/g, '')
+                        );
+                        url = updateParam(url, target, targetVal);
+                    }
 
-                    newParamVal = updateJoinedParameters(
-                        newParamVal,
-                        target,
-                        targetVal.replace(" ", "")
-                    );
-                    url = updateParam(url, target, targetVal);
                 }
-            }
 
-            if (url.indexOf(newParam) === -1) {
-                url = appendParam(url, target, targetVal.replace(" ", ""));
-            } else {
-                updateParam(url, target, targetVal.replace(" ", ""));
+                if (newParam != " ") {
+
+                    if (url.indexOf(newParam) === -1) {
+                        url = appendParam(url, target, targetVal).replace(/\s+/g, '');
+                    } else {
+                        updateParam(url, target, targetVal).replace(/\s+/g, '');
+                    }
+                }
+                //         else if (targetVal === undefined) {
+                //             newParamVal += "-";
+                //         }
+                setCookie(baseParam, newParamVal);
             }
-        } else if (targetVal === undefined) {
-            newParamVal += "-" + appendedParam;
         }
-        setCookie(baseParam, newParamVal);
     }
-
     result = updateParam(url, baseParam, newParamVal);
     return result;
+
 };
